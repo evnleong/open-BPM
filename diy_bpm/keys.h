145aa70e247a49040562713bcb6f279710ecd23a
Evan <epleon36@gmail.com>
2024-05-18 22:42:40 -0400

    Adding keys.h template file

---
+++ diy_bpm/keys.h
@@ -0,0 +1,7 @@
+#include "stdint.h"
+#ifndef KEYS_H
+#define KEYS_H
+const uint8_t PROGMEM APPEUI[8] = { XX, XX, XX, XX, XX, XX, XX, XX };
+const uint8_t PROGMEM DEVEUI[8]= { XX, XX, XX, XX, XX, XX, XX, XX};
+const uint8_t PROGMEM APPKEY[16]= {XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX};
+#endif
